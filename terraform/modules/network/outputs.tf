902113603f39b052101b77ee60160baa553c28e3
Mahmoud Yassen <mahmoudyassen1005@gmail.com>
2025-07-31 18:55:54 +0300

    Update outputs.tf

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,8 +1,14 @@
-output "public_subnet_id" {
-  value = aws_subnet.public.id
+output "vpc_id" {
+  description = "The ID of the created VPC."
+  value       = aws_vpc.main.id
 }
 
+output "public_subnet_id" {
+  description = "ID of the public subnet."
+  value       = aws_subnet.public.id
+}
 
-output "vpc_id" {
-  value = aws_vpc.main.id
+output "private_subnet_id" {
+  description = "ID of the private subnet."
+  value       = aws_subnet.private.id
 }
