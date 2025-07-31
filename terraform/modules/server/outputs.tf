e5261807f85de5db0a46ba26beb55a9590c932fb
Mahmoud Yassen <mahmoudyassen1005@gmail.com>
2025-07-31 18:54:16 +0300

    Update outputs.tf

--- terraform/modules/server/outputs.tf
+++ terraform/modules/server/outputs.tf
@@ -1,8 +1,14 @@
-output "public_ip" {
-  value = aws_instance.jenkins.public_ip
+output "jenkins_master_public_ip" {
+  description = "The public IP address of the Jenkins Master instance."
+  value       = aws_instance.jenkins_master.public_ip
 }
 
-output "jenkins_security_group_id" {
-  description = "The ID of the Jenkins EC2 instance Security Group"
-  value       = aws_security_group.jenkins_sg.id
+output "jenkins_master_private_ip" {
+  description = "The private IP address of the Jenkins Master instance."
+  value       = aws_instance.jenkins_master.private_ip
+}
+
+output "jenkins_slave_private_ip" {
+  description = "The private IP address of the Jenkins Slave instance."
+  value       = aws_instance.jenkins_slave.private_ip
 }
