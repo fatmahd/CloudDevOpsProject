71cc9f29744a9d579b87f9534eec7e67d5da46fd
Mahmoud Yassen <mahmoudyassen1005@gmail.com>
2025-07-31 18:56:15 +0300

    Update variables.tf

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -1,4 +1,19 @@
-variable "project_name" {}
-variable "vpc_cidr" {}
-variable "public_subnet_cidr" {}
-variable "availability_zone" {}
+variable "project_name" {
+  description = "اسم المشروع للـtagging."
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block للـVPC."
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block للـpublic subnet."
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block للـprivate subnet."
+  type        = string
+}
