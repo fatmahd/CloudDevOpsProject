f1ef20c10a217ddf46e03be7921b60072114af74
Mahmoud Yassen <mahmoudyassen1005@gmail.com>
2025-07-31 18:58:10 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,24 @@
+# استدعاء module الشبكة
 module "network" {
-  source              = "./modules/network"
-  project_name        = var.project_name
-  vpc_cidr            = "10.0.0.0/16"
+  source = "./modules/network" # مسار module الشبكة
+
+  project_name = var.project_name
+  vpc_cidr     = "10.0.0.0/16"
   public_subnet_cidr  = "10.0.1.0/24"
-  availability_zone   = "us-east-1a"
+  private_subnet_cidr = "10.0.10.0/24"
 }
 
+# استدعاء module السيرفرات (Jenkins Master and Slave)
 module "server" {
-  source             = "./modules/server"
-  project_name       = var.project_name
-  subnet_id          = module.network.public_subnet_id
-  vpc_id             = module.network.vpc_id
-  key_name           = var.key_name
-  ami_id             = var.ami_id
-  instance_type      = "t2.micro"
-  private_key_path  = var.private_key_path                  # ← وأضف دي
+  source = "./modules/server" # مسار module السيرفرات
 
+  project_name         = var.project_name
+  key_pair_name        = var.key_pair_name
+  ami_id               = var.ami_id
+  master_instance_type = var.master_instance_type
+  slave_instance_type  = var.slave_instance_type
+
+  vpc_id                = module.network.vpc_id
+  public_subnet_id      = module.network.public_subnet_id
+  private_subnet_id     = module.network.private_subnet_id
 }
