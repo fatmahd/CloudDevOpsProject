d64e051673790da30eed5585925da8e5e08c59ba
Mahmoud Yassen <mahmoudyassen1005@gmail.com>
2025-07-31 18:57:09 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,38 @@
 variable "project_name" {
-  default = "CloudDevOpsProject"
+  description = "اسم المشروع للـtagging."
+  type        = string
+  default     = "CloudDevOpsProject"
 }
 
-variable "key_name" {
-  default = "CloudDevOpsProject-keypair"
+variable "aws_region" {
+  description = "الـAWS Region اللي هيتم فيها الـDeployment."
+  type        = string
+  default     = "us-east-1" # **مهم: لازم دي تتطابق مع الـregion في providers.tf**
+}
+
+variable "key_pair_name" {
+  description = "اسم الـKey Pair للدخول على الـEC2 instances."
+  type        = string
+  # **مهم جداً: غير دي لاسم الـKey Pair بتاعك الموجود في AWS**
+  default     = "CloudDevOpsProject-keypair"
 }
 
 variable "ami_id" {
-  default = "ami-0cbbe2c6a1bb2ad63" # Amazon Linux 2023
-}
-variable "private_key_path" {
-  description = "Path to the private key file"
+  description = "الـAMI ID لإنشاء الـEC2 instances (Ubuntu 22.04 LTS recommended)."
   type        = string
+  # **مهم جداً: غير دي لـAMI ID صحيح لمنطقتك ونظام التشغيل اللي هتستخدمه**
+  # تقدر تلاقيه في AWS EC2 Console أو تستخدم data source
+  default     = "ami-0cbbe2c6a1bb2ad63"
+}
+
+variable "master_instance_type" {
+  description = "نوع الـEC2 instance للـJenkins Master."
+  type        = string
+  default     = "t2.medium"
+}
+
+variable "slave_instance_type" {
+  description = "نوع الـEC2 instance للـJenkins Slave."
+  type        = string
+  default     = "t2.medium"
 }
